3ae1de8eb7610a48a612192ea8a6177498b735e4
Asato4931 <asato4931@gmail.com>
2025-02-27 18:25:46 +0900

    Configured Module

--- src/get-estate/get-estate.module.ts
+++ src/get-estate/get-estate.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GetEstateController } from './get-estate.controller';
 import { GetEstateService } from './get-estate.service';
+import { EstateRepository } from './repositories/estate.repository';
 
 @Module({
   controllers: [GetEstateController],
-  providers: [GetEstateService]
+  providers: [GetEstateService, EstateRepository],
 })
 export class GetEstateModule {}
