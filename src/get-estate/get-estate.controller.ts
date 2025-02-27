e4fe72c6abada4dedd5232d410fad60618291317
Asato4931 <asato4931@gmail.com>
2025-02-27 18:27:56 +0900

    Added Controller

--- src/get-estate/get-estate.controller.ts
+++ src/get-estate/get-estate.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Query,
+  Get,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { GetEstateService } from './get-estate.service';
+import { GetEstateQueryDto } from './dto/get-estatequery.dto';
+import {} from 'express';
 
-@Controller('get-estate')
-export class GetEstateController {}
+@Controller('api/v1/townPlanning/estateTransaction/bar')
+export class GetEstateController {
+  constructor(private getEstateService: GetEstateService) {}
+  @Get()
+  @UsePipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  )
+  findAll(@Query() query: GetEstateQueryDto): number[] | string {
+    const { year, prefectureCode, type } = query;
+    return this.getEstateService.getEstates(year, prefectureCode, type);
+  }
+}
